7eb408d7b93291af0e21e32d20151c85cfaf44f1
Witecki,Amanda <Amanda.Witecki@Cerner.com>
2020-03-06 11:20:32 -0500

    Updates

---
+++ precache-manifest.c9c5404479164cd7fd5439c7d56f9129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac4a81bf10672d763a249c326a384172",
+    "url": "/myapp2/index.html"
+  },
+  {
+    "revision": "6187ed1385f6b97dccb8",
+    "url": "/myapp2/static/css/main.2063f148.chunk.css"
+  },
+  {
+    "revision": "ca8182b4bc6c7373c6d2",
+    "url": "/myapp2/static/js/2.76546421.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/myapp2/static/js/2.76546421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6187ed1385f6b97dccb8",
+    "url": "/myapp2/static/js/main.ae6f22a8.chunk.js"
+  },
+  {
+    "revision": "536cf98fed10523f9424",
+    "url": "/myapp2/static/js/runtime-main.9b2f9fb9.js"
+  }
+]);
